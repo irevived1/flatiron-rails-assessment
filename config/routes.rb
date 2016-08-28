c398809e1f1ea6e87b8ceba4da1eb6e33701509e
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-28 13:05:14 -0400

    facebook login available

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'static/index'
+  get 'static/about'
+
+  root to: 'static#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
