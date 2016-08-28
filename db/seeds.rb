22382435a3d2f6ac81c297810cd4c51b6816e1c5
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-27 21:53:11 -0400

    devise added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(email:"a@b.c",password:"12341234",password_confirmation:"12341234")
