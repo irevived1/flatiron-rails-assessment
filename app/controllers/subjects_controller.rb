a4481a9fed4704f06c2b211a03abd509c3705a6d
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-25 00:15:46 -0400

    fixed path restriction

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,17 +1,21 @@
 class SubjectsController < ApplicationController
   def index
     @subjects = current_user.subjects.uniq
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @subject}
+    end
   end
 
   def show
     @subject = Subject.find_by(id:params[:id])
-    # unless @subject
-      # return redirect_to subjects_path
-      respond_to do |format|
-        format.html { render :show }
-        format.json { render json: @subject}
-      end
-    # end
+    unless @subject
+      return redirect_to subjects_path
+    end
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @subject}
+    end
   end
 
 end
